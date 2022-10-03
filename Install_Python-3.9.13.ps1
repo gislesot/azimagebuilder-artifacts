71116f363f3abe61780d5661257cbbd5dbc34f02
Gisle <gisle@soterud.no>
2022-10-03 13:00:43 +0200

    Update Install_Python-3.9.13.ps1
    Add sleep inbetween command

--- Install_Python-3.9.13.ps1
+++ Install_Python-3.9.13.ps1
@@ -1,8 +1,8 @@
-Write-Output ">>> Creating new directory C:\buildArtifacts"
+Write-Output "Creating new directory C:\buildArtifacts"
 New-Item -ItemType Directory C:\buildArtifacts
-
-Write-Output ">>> Downloading Python 3.9.13 ..."
+Sleep 2
+Write-Output "Downloading Python 3.9.13 ..."
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe" -OutFile "c:\buildArtifacts\python-3.9.13-amd64.exe"
-
-Write-Output ">>> Running silent Python install ..."
+Sleep 2
+Write-Output "Running silent Python install ..."
 Start-Process "c:\buildArtifacts\python-3.9.13-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1"
