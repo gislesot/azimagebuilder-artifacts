d9c0865e8272ccf695e4d340cd263d07011a6f18
Gisle <gisle@soterud.no>
2022-10-03 14:59:46 +0200

    Make start-process wait for finished installation

--- Install_Python-3.9.13.ps1
+++ Install_Python-3.9.13.ps1
@@ -5,4 +5,5 @@ Write-Output "Downloading Python 3.9.13 ..."
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe" -OutFile "c:\buildArtifacts\python-3.9.13-amd64.exe"
 Sleep 2
 Write-Output "Running silent Python install ..."
-Start-Process "c:\buildArtifacts\python-3.9.13-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1"
+Start-Process "c:\buildArtifacts\python-3.9.13-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+Write-Output "Installig Python finished"
