17b8eedcd4dd5268d69b341d72d1d44a7772c92a
Gisle <gisle@soterud.no>
2022-10-03 16:08:57 +0200

    Add sleep after starting installation of python

--- Install_Python-3.9.13.ps1
+++ Install_Python-3.9.13.ps1
@@ -6,4 +6,5 @@ Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.13/python-3.9.13-a
 Sleep 2
 Write-Output "Running silent Python install ..."
 Start-Process "c:\buildArtifacts\python-3.9.13-amd64.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-Write-Output "Installig Python finished"
+Write-Output "Sleeping 30 seconds ..."
+Sleep 30
